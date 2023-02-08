8d1764d896e5051dbf3efcfaa8382349822b57fd
francocerino <francocerino@gmail.com>
2023-02-08 19:56:14 -0300

    otro test a surrogate

--- tests/test_surrogate.py
+++ tests/test_surrogate.py
@@ -10,6 +10,8 @@ import numpy as np
 
 from skreducedmodel.reducedbasis import error
 
+from scipy.special import jv as BesselJ
+
 
 def test_rom_rb_interface(rom_parameters):
     """Test API consistency."""
@@ -84,3 +86,30 @@ def test_predictions_dataset_real_dataset(ts_train,
         errors_rom.append(error(h, h_rom, times))
 
     assert errors_f_model == errors_rom
+
+
+def test_surrogate_accuracy():
+    """Test surrogate accuracy for Bessel functions."""
+
+    parameter_points = np.linspace(1, 10, num=101)
+    nu_validation = np.linspace(1, 10, num=1001)
+    physical_points = np.linspace(0, 1, 1001)
+
+    # build training space
+    training = np.array(
+        [BesselJ(nn, physical_points) for nn in parameter_points]
+    )
+
+    # build reduced basis
+    rom = Surrogate(greedy_tol=1e-15)
+    rom.fit(training_set = training,
+            physical_points = physical_points,
+            parameters = parameter_points
+            )
+
+    bessel_test = [BesselJ(nn, physical_points) for nn in nu_validation]
+    bessel_surrogate = [rom.predict(nn) for nn in nu_validation]
+
+    np.testing.assert_allclose(
+        bessel_test, bessel_surrogate, rtol=1e-5, atol=1e-5
+    )
