b5666e2301f8297793ce78e474c1627616281c30
francocerino <francocerino@gmail.com>
2023-02-22 21:34:38 -0300

    agregando tests para mksurrogate

--- tests/test_mksurrogate.py
+++ tests/test_mksurrogate.py
@@ -11,6 +11,12 @@ def test_error_non_trained_rb_only():
         mksurrogate(instance=rb)
 
 
+def test_wrong_parameter():
+    # debe devolver: InputError("something must not be given. It
+    # is not a parameter to use.)"
+    with pytest.raises(InputError):
+        mksurrogate(something=2)
+
 def test_instance_class():
     with pytest.raises(InputError):
         eim = EmpiricalInterpolation()
@@ -69,3 +75,30 @@ def test_rb_given_and_hyperparameters_of_it(ts_train, parameters_train, times):
             physical_points=times,
         )
         mksurrogate(rb, lmax=1)
+
+def test_rb_given_all_ok(ts_train, parameters_train, times):
+    rb = ReducedBasis()
+    rb.fit(
+        parameters=parameters_train[:, 0],
+        training_set=ts_train,
+        physical_points=times,
+    )
+    surrogate = mksurrogate(rb)
+    surrogate.predict(1)
+
+def test_eim_given_all_ok(ts_train, parameters_train, times):
+    rb = ReducedBasis()
+    rb.fit(
+        parameters=parameters_train[:, 0],
+        training_set=ts_train,
+        physical_points=times,
+    )
+    eim = EmpiricalInterpolation(rb)
+    surrogate = mksurrogate(eim)
+    surrogate.predict(1)
+
+def test_eim_with_no_training_data_fails():
+    with pytest.raises(InputError):
+        rb = ReducedBasis()
+        eim = EmpiricalInterpolation(rb)
+        surrogate = mksurrogate(eim)
