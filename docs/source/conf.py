1d32e100ad2d0a07b1f74a062dfffb48b520fe64
AgusMRM <agusrodriguezmedrano@gmail.com>
2023-02-24 11:10:16 -0300

    asd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,13 +6,7 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
 import sys
-##################### 
-import mock
- 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-#####################
+
 sys.path.insert(0, os.path.abspath("../.."))
 
 # import skreducemodel
