7ee8efe1351c02c0925beac75c480bb7ac82efa8
AgusMRM <agusrodriguemedrano@gmail.com>
2023-02-24 21:07:52 -0300

    modifique el conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
-# import skreducemodel
+import skreducedmodel
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
